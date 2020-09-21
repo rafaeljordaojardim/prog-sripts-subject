6d192d0da4be1d9a71c07589573cb5ef3d5aa7ac
rodrimartins <rodrimartins2005@gmail.com>
2020-09-21 19:40:45 -0300

    Aula de Prog. Scripts - 21/09/2020

---
+++ Módulo3-Exemplos/js/exemplo3.js
@@ -0,0 +1,20 @@
+/*
+função anônima
+var quadrado = function (numero) {
+  return numero * numero;
+}
+*/
+
+// arrow functions
+var quadrado = (numero) => {
+  return numero * numero;
+}
+
+function receber() {
+  var n1 = quadrado(parseInt(document.getElementById('num1').value));
+  document.write("<h1>Resultado:</h1>" + "<h2>" + n1 + "</h2>");
+}
+
+
+
+
