133ec88d0a672ae5c916c68c938e4bcf2d57777f
rodrimartins <rodrimartins2005@gmail.com>
2020-11-09 07:53:12 -0300

    Atualização de bug do dia 07/11/2020

--- JS02-Projeto/categories/CategoriesController.js
+++ JS02-Projeto/categories/CategoriesController.js
@@ -7,7 +7,8 @@ router.get("/categories", (req, res) =>{
 });
 
 router.get("/admin/categories/new", (req, res) =>{
-    res.render("new");
+    res.render("admin/categories/new");
 });
 
 module.exports = router;
+
