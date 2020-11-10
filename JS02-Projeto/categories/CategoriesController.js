6001ac1722e5d77a10cf2028a6ad68a6fb5bab0c
rodrimartins <rodrimartins2005@gmail.com>
2020-11-10 10:36:00 -0300

    Atualização de bug do dia 09/11/2020

--- JS02-Projeto/categories/CategoriesController.js
+++ JS02-Projeto/categories/CategoriesController.js
@@ -1,14 +1,97 @@
-
+//definindo as rotas relacionadas as categorias com o express.router
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) =>{
-    res.sender("new");
-});
+//carregando o Model de Categoria
+const Category = require("./Category");
 
-router.get("/admin/categories/new", (req, res) =>{
+//carregando a bliblioteca slugify
+const slugify = require("slugify");
+
+
+router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 });
 
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        Category.create({ //insert into
+            title: title,
+            slug: slugify(title) //Title -> "Programação de Scripts" com slug vai transformar em "programacao-de-scripts" para ser possível usar nas rotas
+        }).then(() => {
+            res.redirect("/admin/categories");
+        });
+    }
+    else {
+        res.redirect("admin/categories/new");
+    }
+});
+
+//criando a rota para listar as categorias
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories });
+    });
+
+});
+
+//criando a rota para exclusão
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {   //se id != null
+        if (!isNaN(id)) {       //se id não for número
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }
+    else {
+        res.redirect("/admin/categories");
+    }
+});
+
+//criando a rota para edição
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category });
+        }
+        else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    });
+});
+
+//criando a rota para update
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    Category.update({ title: title, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    });
+});
+
+//exportando a variável router para poder ser linkado no arquivo principal (index.js)
 module.exports = router;
+
+
 
